e3df401ebbbac1cd351e9eb3f7933e77a7ac972a
Patrick Ofili <patrickofili16@gmail.com>
2020-05-10 04:18:20 +0100

    fixed back button

--- src/components/FullCont.js
+++ src/components/FullCont.js
@@ -1,8 +1,7 @@
 import React from "react";
 import MainCont from "./MainCont";
 import Footer from "./Footer";
-import { useSelector, useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
+
 
 const FullCont = () => {
   return (
