0ae83d2227fb2cbf633aae4dffcf168abe973d4c
Patrick Ofili <patrickofili16@gmail.com>
2020-07-02 02:08:11 +0100

    added API endpoint

--- src/actions/mainContActions.js
+++ src/actions/mainContActions.js
@@ -4,16 +4,14 @@ import axios from "axios";
 export const calculatePipeSize = (sizingInfo) => (dispatch) => {
   console.log(sizingInfo);
   axios
-    .post("http://localhost:5000/api/results", sizingInfo)
+    .post("https://pied-backend.herokuapp.com/api/results", sizingInfo)
     .then((res) => {
       dispatch({
         type: CALCULATE_PIPE_SIZE,
         payload: res.data,
-      })
+      });
     })
-    .catch(err => {
-  console.log(err)
-})  
-  
+    .catch((err) => {
+      console.log(err);
+    });
 };
-
