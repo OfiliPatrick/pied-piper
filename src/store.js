392a2b8ae9f5893363c9c82c4b9aa7f91d65205d
Patrick Ofili <patrickofili16@gmail.com>
2020-07-04 03:17:42 +0100

    updated store

--- src/store.js
+++ src/store.js
@@ -10,8 +10,9 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
+    //enable in dev
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
